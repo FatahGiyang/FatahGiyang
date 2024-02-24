1093acab6bad7a3da706b0c028ef50427deb0723
FatahAja <160618941+FatahGiyang@users.noreply.github.com>
2024-02-24 09:43:17 +0700

    Update KCT.go

--- KCT.go
+++ KCT.go
@@ -76,7 +76,7 @@ func main() {
 	t := os.Getenv("HULKMAXPROCS")
 	maxproc, err := strconv.Atoi(t)
 	if err != nil {
-		maxproc = 150000000000
+		maxproc = 150000000
 	}
 
 	u, err := url.Parse(site)
